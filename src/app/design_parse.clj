4eb4560d63cfbc14fe6260153e819b1b8596a2d2
Daan Reid <osmosisch@gmail.com>
2017-07-14 12:16:16 +0200

    Masking parse + test

--- src/app/design_parse.clj
+++ src/app/design_parse.clj
@@ -6,19 +6,39 @@
   (insta/parser
     "masking = noMasking | singleBlind | doubleBlind
      noMasking = 'No masking' | 'Open Label'
-     singleBlind = 'Single Blind' | 'Single Blind ' <paropen> (who | unknown) <parclose> | who  
-     doubleBlind = 'Double-Blind' | 'Double Blind ' <paropen> multipleWho <parclose>
+     singleBlind = <'Single Blind'> | <'Single Blind'> <space>+ <paropen> (who | unknown) <parclose> | who
+     doubleBlind = <'Double-Blind'> | <'Double Blind'> <space>+ <paropen> multipleWho <parclose> | multipleWho
      multipleWho = 'masked roles unspecified' | who <sep> (who <sep>?)+
      sep = ', '
-     who = 'Care Provider' | 'Investigator' | 'Outcomes Assessor'
+     space = ' '
+     paropen = '('
+     parclose = ')'
+     who = 'Care Provider' | 'Investigator' | 'Outcomes Assessor' | 'Participant'
      unknown = 'masked role unspecified'"))
+
+(defn key-to-label [the-key]
+  (let [masking-types {:singleBlind "Single Blind"
+                       :doubleBlind "Double Blind"
+                       :noMasking "No masking"}]
+    (the-key masking-types)))
+
+(defn get-extra-terms [masking-spec blinding]
+  (case blinding
+    "Single Blind" (if (> (count masking-spec) 1)
+                     (rest (second masking-spec))
+                     `())
+    "Double Blind" (if (> (count masking-spec) 1)
+                     (map second (rest (nth masking-spec 1)))
+                     `())
+    "No masking" `()))
+
 (defn parse-masking [the-str]
   (if (= "N/A" the-str)
     "N/A"
     (let
       [masking-map (masking the-str)
-       blinding (second (second masking-map))
-       extra-terms (map #(second %1) (rest (nth masking-map 2)))]
-
+       masking-spec (second masking-map)
+       blinding (key-to-label (first masking-spec))
+       extra-terms (get-extra-terms masking-spec blinding)]
       (concat (list blinding) extra-terms))))
 
--- test/app/design_parse_test.clj
+++ test/app/design_parse_test.clj
@@ -1,4 +1,5 @@
 (ns app.design-parse-test
+  (:use clojure.java.io)
   (:use clojure.test)
   (:use app.design-parse))
 
@@ -10,12 +11,31 @@
   (is (= (parse-masking "Single Blind (Outcomes Assessor)")
          ["Single Blind" "Outcomes Assessor"])))
 
-(deftest test-masking-parser-single-blind-single-spec
+(deftest test-masking-parser-single-blind-spec-only
+  (is (= (parse-masking "Care provider")
+         ["Single Blind" "Outcomes Assessor"])))
+
+(deftest test-masking-parser-single-blind-spec-only
   (is (= (parse-masking "Outcomes Assessor")
          ["Single Blind" "Outcomes Assessor"])))
 
+(deftest test-masking-parser-single-blind-no-spec
+  (is (= (parse-masking "Single Blind")
+         ["Single Blind"])))
 
-; (deftest test-masking-parser-no-blinding
-;   (is (= (parse-masking "Participant, Investigator")
-;     ["Participant" "Investigator"]))
-;   )
+(deftest test-masking-parser-no-blinding
+  (is (= (parse-masking "Participant, Investigator")
+    ["Double Blind" "Participant" "Investigator"])))
+
+(deftest test-masking-double-blind-no-specs
+  (is (= (parse-masking "Double-Blind")
+         ["Double Blind"])))
+
+(deftest test-no-masking
+  (is (= (parse-masking "No masking")
+         ["No masking"])))
+
+(deftest test-parse-all-no-exceptions
+  (is (nil? (with-open [rdr (reader "test/app/knownMaskings.txt")]
+        (doseq [line (line-seq rdr)]
+          (parse-masking line))))))
